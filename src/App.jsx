8e0d33e8f4d633d583a695c13751153c58697c05
Aryan-tiwari <aryant@iitbhilai.ac.in>
2024-12-23 15:35:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
+  }
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
+    setTodos(newTodoList);
+  }
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index;
+    });
+    persistData(newTodoList);
+    setTodos(newTodoList);
+  }
+  function handleEditTodo(index) {
+    const editTodo = todos[index];
+    setTodoValue(editTodo);
+    handleDeleteTodo(index);
+  }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+    let localTodos = localStorage.getItem("todos");
+    if (!localTodos) {
+      return;
+    }
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []); //to run on page load
+  return (
+    <>
+      <TodoInput
+        handleAddTodos={handleAddTodos}
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+      />
+      <TodoList
+        handleEditTodo={handleEditTodo}
+        handleDeleteTodo={handleDeleteTodo}
+        todos={todos}
+      />
+    </>
+  );
+}
+
+export default App;
